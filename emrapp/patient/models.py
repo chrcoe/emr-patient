f64eb9fc8d78c2bc5a368128eeff48b38d295f7e
willim16 <willim16@email.franklin.edu>
2014-10-03 19:30:38 -0400

    Cleaned up tabs to spaces.

--- emrapp/patient/models.py
+++ emrapp/patient/models.py
@@ -1,5 +1,5 @@
 from django.db import models
- 
+
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, UserManager
 from django.conf import settings
 from django.db import models

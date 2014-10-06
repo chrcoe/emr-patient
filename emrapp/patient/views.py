7817da4867a92c69bc909070864f51ce3e00197f
Robert Duncan <duncan17@email.franklin.edu>
2014-10-06 13:24:41 -0400

    Import added for 404

--- emrapp/patient/views.py
+++ emrapp/patient/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render get_object_or_404
+from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 
 def index(request):
         return HttpResponse("Hello, world. You're at the PATIENT index.")

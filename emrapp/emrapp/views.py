aa220171ec567c35196e634cd4fa57605b4296c6
willim16 <willim16@email.franklin.edu>
2014-10-12 19:31:21 -0400

    Created initial patient login page.

--- emrapp/emrapp/views.py
+++ emrapp/emrapp/views.py
@@ -1,7 +1,22 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, render_to_response
+from django.contrib.auth import authenticate, login
 
 def login(request):
-    return render(request, 'main/login.html',
-                  {'page_name': 'LOGIN'})
+    state = "Please enter Username and Password"
+    username = password = ''
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                state = "Logged in."
+            else:
+                state = "Account is not active. Contact your system administrator."
+        else:
+            state = "Your username and/or password were incorrect."
+
+    return render_to_response('main/base-login.html',{'state':state, 'username': username})

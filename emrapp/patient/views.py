7db3841d48d67d609bd04bded4938a808ea5dd15
willim16 <willim16@email.franklin.edu>
2014-11-01 18:25:15 -0400

    Created settings page, and updated medication page.

--- emrapp/patient/views.py
+++ emrapp/patient/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, get_list_or_404
 from django.shortcuts import render
+from django.contrib import messages
 
 from patient.models import Allergy
 from patient.models import Appointment
@@ -96,6 +97,18 @@ def medication(request, patient_id):
     medication = get_list_or_404(Medication, id_patient=patient_id)
     if patient.is_admin or patient.is_staff:
         return redirect('/admin/')
+
+    # need to handle the POST data for all notes
+    if request.method == 'POST':
+        #         posted = request.POST
+        for i in medication:
+            # update ONLY the notes that the patient wanted to update
+            if i.id == int(request.POST['medication_id']):
+                # set the notes on THIS record to the new notes
+                i.medication_notes = request.POST['medication_notes']
+                # save changes to the DB
+                i.save()
+				
     return render(request, 'patient/medication.html',
                   {'patient': patient, 'medication': medication, 'page_name': 'Medication'})
 
@@ -173,5 +186,23 @@ def settings(request, patient_id):
     patient = get_object_or_404(Patient, pk=patient_id)
     if patient.is_admin or patient.is_staff:
         return redirect('/admin/')
+		
+    # need to handle the POST data for all info
+    if request.method == 'POST':
+        # set the info on THIS record to the new info
+        if 'phone_num' in request.POST:
+            patient.phone_num = request.POST['phone_num']
+        if 'street_address' in request.POST:
+            patient.street_address = request.POST['street_address']
+        if 'city' in request.POST:
+            patient.city = request.POST['city']
+        if 'state' in request.POST:
+            patient.state = request.POST['state']
+        if 'zip_code' in request.POST:
+            patient.zip_code = request.POST['zip_code']
+        # save changes to the DB
+        patient.save()
+        messages.success(request, 'Saved changes.')
+				
     return render(request, 'patient/settings.html',
                   {'patient': patient, 'settings': settings, 'page_name': 'Settings'})
